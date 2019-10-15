92abb376ec6dc69ce5463b94d50e7d33fb60e9fd
Administrator <jean.pan9@qq.com>
2019-10-15 22:11:00 +0800

    init

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,10 @@
 import {get} from './helper'
 
 const getSeller = get('/api/seller')
+const getGoods = get('/api/goods')
+const getRatings = get('/api/ratings')
 export {
-  getSeller
+  getSeller,
+  getGoods,
+  getRatings
 }
--- frontend/src/components/header/index.vue
+++ frontend/src/components/header/index.vue
@@ -37,16 +37,20 @@
           <div class="star-wrapper">
             <Star :size="48" :score="seller.score"></Star>
           </div>
-          <div class="titel">
-            <div class="line">
-            </div>
-            <div class="text">优惠信息</div>
-            <div class="line">
-            </div>
+          <Title :title="'优惠信息'"></Title>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="item in seller.supports" :key="item">
+              <span class="icon" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
+          <Title :title="'商家公告'"></Title>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
           </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" v-on:click="hideDetail">
         <i class="icon-close"></i>
       </div>
     </div>
@@ -56,6 +60,7 @@
 <script>
 import {getSeller} from 'api'
 import Star from 'components/star/index'
+import Title from 'components/title/index'
 
 export default {
   data () {
@@ -68,6 +73,9 @@ export default {
   methods: {
     showDetail () {
       this.detailShow = true
+    },
+    hideDetail () {
+      this.detailShow = false
     }
   },
   created () {
@@ -77,7 +85,8 @@ export default {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   },
   components: {
-    Star
+    Star,
+    Title
   }
 }
 </script>
@@ -156,7 +165,7 @@ export default {
 
           .text
             line-height: 12px
-            font-size: 10px
+            font-size: 12px
 
       .support-count
         position: absolute
@@ -247,20 +256,54 @@ export default {
             padding: 2px 0
             text-align: center
 
-          .titel
-            display: flex
+          .supports
             width: 80%
-            margin: 30px auto 24px auto
+            margin: 0 auto
 
-            .line
-              flex: 1
-              position: relative
-              top: -6px
-              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
-
-            .text
+            .support-item
               padding: 0 12px
-              font-size: 14px
+              margin-bottom: 12px
+              font-size: 0
+
+              &:last-child
+                margin-bottom: 0
+
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+
+                &.decrease
+                  bg-image('decrease_2')
+
+                &.discount
+                  bg-image('discount_2')
+
+                &.guarantee
+                  bg-image('guarantee_2')
+
+                &.invoice
+                  bg-image('invoice_2')
+
+                &.special
+                  bg-image('special_2')
+
+              .text
+                line-height: 16px
+                font-size: 12px
+
+          .bulletin
+            width: 80%
+            margin: 0 auto
+
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
 
       .detail-close
         position: relative
